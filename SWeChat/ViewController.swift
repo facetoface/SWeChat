4a1ec365ea04164ffed4556bd820423085440fd4
EasyIOS <1392984568@QQ.com>
2017-01-03 15:20:47 +0800

    Initial Commit

---
+++ SWeChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SWeChat
+//
+//  Created by ChiCo on 17/1/3.
+//  Copyright © 2017年 ChiCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
