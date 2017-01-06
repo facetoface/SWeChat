7710191696da7e470ea5af8a63351e66a851b11f
EasyIOS <1392984568@QQ.com>
2017-01-06 18:04:33 +0800

    开始做聊天界面

--- SWeChat/Classes/Chat/QKChatViewController.swift
+++ SWeChat/Classes/Chat/QKChatViewController.swift
@@ -10,27 +10,28 @@ import UIKit
 
 class QKChatViewController: UIViewController {
     var messageModel: MessageModel?
-
+    @IBOutlet var refreshView: UIView!
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    lazy var listTableView: UITableView = {
+        let listTableView = UITableView(frame: CGRect.zero, style: .plain)
+//        listTableView.dataSource = self
+//        listTableView.delegate = self
+        listTableView.backgroundColor = UIColor.clear
+        listTableView.separatorStyle = .none
+        listTableView.backgroundView = UIImageView(image: TSAsset.Chat_background.image)
+        return listTableView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
