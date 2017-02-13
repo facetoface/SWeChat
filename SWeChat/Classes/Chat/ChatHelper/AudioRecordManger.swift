ce7381b7bc54b293e8b26f046536d61a5fd770ae
EasyIOS <1392984568@QQ.com>
2017-02-13 18:02:42 +0800

    开始看录音

---
+++ SWeChat/Classes/Chat/ChatHelper/AudioRecordManger.swift
@@ -0,0 +1,13 @@
+//
+//  AudioRecordManger.swift
+//  SWeChat
+//
+//  Created by ChiCo on 17/2/13.
+//  Copyright © 2017年 ChiCo. All rights reserved.
+//
+
+import UIKit
+
+class AudioRecordManger: NSObject {
+
+}
